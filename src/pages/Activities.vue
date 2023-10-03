d06a93ac85e113fc5ed6d7170f922e2ffacfe836
Elian Guananjay <127788667+elianguana@users.noreply.github.com>
2023-10-03 16:46:57 -0300

    actividades-parrafo

--- src/pages/Activities.vue
+++ src/pages/Activities.vue
@@ -5,5 +5,8 @@ export default {
 </script>
 
 <template>
-    <h1 class="mb-4 text-3xl">Activities</h1>
+
+    <h1 class="mb-4 text-3xl">Activities</h1> 
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ad soluta labore quidem debitis doloremque veritatis temporibus? Cum fuga tempora tenetur autem, quos id eos voluptatibus officiis! Eveniet, explicabo nihil!</p>
 </template>
