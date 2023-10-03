26c3e89f71d9d0c38e229363816826f7275643ec
Elian Guananjay <127788667+elianguana@users.noreply.github.com>
2023-10-03 16:49:29 -0300

    otro parrafo

--- src/pages/Activities.vue
+++ src/pages/Activities.vue
@@ -9,4 +9,6 @@ export default {
     <h1 class="mb-4 text-3xl">Activities</h1> 
 
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus ad soluta labore quidem debitis doloremque veritatis temporibus? Cum fuga tempora tenetur autem, quos id eos voluptatibus officiis! Eveniet, explicabo nihil!</p>
+
+    <p><strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quibusdam eveniet temporibus cupiditate, tenetur incidunt sed officia laboriosam alias dolore voluptates eos odio esse eius fugiat libero magnam odit placeat.</strong></p>
 </template>
