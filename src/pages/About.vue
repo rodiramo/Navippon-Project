80b6d87c86e97721133008b6b067eac30ba3708f
Elian Guananjay <127788667+elianguana@users.noreply.github.com>
2023-10-20 21:51:56 -0300

    home y about traducido al inglish
    ya traduci al ingles el home y al about

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,28 +9,25 @@ export default {
         <h1 class="mb-4 text-3xl">Abous Us</h1>
     </section>
     <section class="col-md-6 container" id="about">
-        <h2 class="text-center h2 mb-3 mt-5">Sobre Nosotros</h2>
+        <h2 class="text-center h2 mb-3 mt-5">Our Venture</h2>
     
         <div class="clearfix mt-4">
 
             <img src="/public/about-img.jpeg" class="col-md-6 float-md-end mb-3 ms-md-3 img-fluid" alt="imagen">
 
             <p>
-                Bienvenidos a <strong>Navippon</strong>, tu guía para descubrir la belleza y la cultura de Japón.
+                Welcome to <strong>Navippon</strong>, your guide to discovering the beauty and culture of Japan.
             </p>
             <p>
-                En Navippon, nos apasiona compartir la magia de Japón contigo.
-                Nuestra misión es brindarte experiencias únicas y memorables en este hermoso país. Con años de experiencia en la industria del turismo, estamos comprometidos a proporcionarte aventuras inolvidables y a ayudarte a explorar Japón de una manera auténtica y significativa.
-            </p>
-
-            <p>
-                Con una historia rica en viajes por Japón y una profunda comprensión de la cultura local, Navipon se enorgullece de ofrecer un nivel excepcional de conocimiento y experiencia.
-                Cada uno de nuestros itinerarios ha sido cuidadosamente diseñado para garantizar una experiencia sin complicaciones, permitiéndote sumergirte completamente en la belleza y la autenticidad de Japón.
+                At Navippon, we are passionate about sharing the magic of Japan with you. Our mission is to provide you with unique and memorable experiences in this beautiful country. With years of experience in the tourism industry, we are dedicated to offering unforgettable adventures and helping you explore Japan in an authentic and meaningful way.
             </p>
 
             <p>
-                Nuestro equipo de expertos en viajes está dedicado a diseñar itinerarios personalizados que se adaptan a tus intereses y necesidades. Ya sea que estés interesado en la <strong>cultura, la historia, la gastronomía o la naturaleza de Japón</strong>, tenemos la experiencia para llevarte a donde deseas ir.
-                Únete a nosotros en esta emocionante aventura y descubre la magia de Japón como nunca antes.
+                With a rich history of traveling in Japan and a deep understanding of the local culture, Navippon takes pride in delivering an exceptional level of knowledge and expertise. Each of our itineraries has been carefully designed to ensure a hassle-free experience, allowing you to fully immerse yourself in the beauty and authenticity of Japan.
+            </p>
+
+            <p>
+                Our team of travel experts is dedicated to creating customized itineraries that cater to your interests and needs. Whether you are interested in <strong>the culture, history, cuisine, or nature </strong> of Japan, we have the expertise to take you where you want to go. Join us on this exciting adventure and discover the magic of Japan like never before.
             </p>
            
         </div>
@@ -39,10 +36,10 @@ export default {
     
     <section class="container mt-5 mb-5">
         
-        <h2 class="text-center h2">Lo que Ofrecemos</h2>
+        <h2 class="text-center h2">What We Offer</h2>
 
         <p class="container text-center">
-         En Navipon, sabemos que cada viajero es único. Por eso, ofrecemos una amplia gama de servicios adaptados a tus preferencias. Desde itinerarios a medida que satisfacen tus intereses específicos, hasta experiencias culturales auténticas que te acercan a la esencia de Japón, estamos listos para superar tus expectativas. Nuestros alojamientos van desde lujosos hoteles en el corazón de la ciudad hasta tradicionales ryokans en hermosos entornos rurales.
+            At <strong> Navippon</strong>, we understand that every traveler is unique. That's why we offer a wide range of services tailored to your preferences. From custom itineraries that cater to your specific interests to authentic cultural experiences that bring you closer to the essence of Japan, we are ready to exceed your expectations. Our accommodations range from luxurious city-center hotels to traditional ryokans in beautiful rural settings. 
         </p>
         
     </section>
